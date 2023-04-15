67bb88b9641ad02c8bff3944da01694999ea0086
arseniIvankovich <arseniIvankovich@gmail.com>
2023-04-15 19:59:05 +0300

    Add connection to the DB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import by.fpmibsu.Dao.DaoImpl.RoleDaoImpl;
+import by.fpmibsu.Entity.Role;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Role> roles = new RoleDaoImpl().findAll();
+        System.out.println(roles);
     }
 }
