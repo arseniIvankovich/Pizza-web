b33a0e00162690052eea727132d71f73052b4363
arseniIvankovich <arseniIvankovich@gmail.com>
2023-04-16 19:38:46 +0300

    Refactor UserDaoImpl

--- src/main/java/by/fpmibsu/Dao/UserDao.java
+++ src/main/java/by/fpmibsu/Dao/UserDao.java
@@ -7,5 +7,7 @@ import java.sql.SQLException;
 public interface UserDao extends BaseDao<Long, User>{
     User findByName (String string) throws SQLException;
 
+    User findByEmail (String email) throws SQLException;
+
     User checkLogin (String email, String password) throws SQLException;
 }
