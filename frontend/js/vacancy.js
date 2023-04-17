f539579a41ee8cd83a76bc3c4dd2e93f858a73d3
alexennk <alexennk@yandex.by>
2023-04-17 22:43:56 +0300

    Fill vacancy header with more information

--- frontend/js/vacancy.js
+++ frontend/js/vacancy.js
@@ -1,3 +1,23 @@
+
+function loadData() {
+    var busket = document.querySelector(".busket-button-text-orig");
+    var busket_quantity = document.querySelector(".busket-button-quantity-text");
+    if (typeof localStorage["busket-price"] === 'undefined') {
+        localStorage["busket-price"] = '0.00';
+    }
+    if (typeof localStorage["busket-quantity"] === 'undefined') {
+        localStorage["busket-quantity"] = '0';
+    }
+    busket.textContent = localStorage["busket-price"] + " BYN";
+    busket_quantity.textContent = localStorage["busket-quantity"];
+}
+
+//localStorage.clear();
+
+loadData();
+
+
+
 const element1 = document.querySelectorAll(".login-form-select");
 
 element1.forEach(element => {
