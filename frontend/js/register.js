a3332b6f0e1f1e0480e450f6494a1b106d570bb6
Alexennk <89784946+Alexennk@users.noreply.github.com>
2023-04-18 21:32:57 +0300

    Update register.js

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -4,5 +4,4 @@ let button = document.querySelector('.save-button');
 button.addEventListener("click", function () {
     localStorage["is-logged-in"] = 'true';
     window.open("../../index.html", "_self");
-    window.open("https://ya.ru/", "_self");
 });
