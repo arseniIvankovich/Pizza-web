0bd6bbfee349fa6b2d086e81798f2629a8d2fa02
arseniIvankovich <arseniIvankovich@gmail.com>
2023-04-29 21:34:17 +0300

    add index.jsp

--- src/main/java/by/fpmibsu/Servlet/GetServlet.java
+++ src/main/java/by/fpmibsu/Servlet/GetServlet.java
@@ -1,14 +1,17 @@
 package by.fpmibsu.Servlet;
 
 
+import javax.naming.Name;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("")
 public class GetServlet extends HttpServlet {
-    private static String index = "/WEB-INF/view/index.jsp";
+    private static String index = "/jsp/index.jsp";
 
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher(index).forward(req,resp);
