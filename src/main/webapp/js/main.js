595e4fafb8736c94f13711fed089ef9bda7066ca
alexennk <alexennk@yandex.by>
2023-05-30 19:06:14 +0300

    add alerts for being not logged in

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -170,119 +170,129 @@ var drink_buskets = document.querySelectorAll(".drink-button");
 
 pizza_buskets.forEach(element => {
     element.addEventListener("click", function() {
-        const element_id = element.id.charAt(7);
-        const price_name = "price-" + element_id;
-        let pizza_card = document.getElementById("pizza-" + element_id);
-        let object = JSON.parse(localStorage["pizza"]);
-        let flag = false;
-        let ind = 0;
-        let bool_size1 = true, bool_dough1 = true;
-        if (pizza_card.querySelector(".size-select").options[0].textContent === "Большой") bool_size1 = false;
-        if (pizza_card.querySelector(".dough-select").options[0].textContent === "Толстое") bool_size1 = false;
-        for (let i = 0; i < object.length; i++) {
-            if (object[i].name === pizza_card.querySelector(".pizza-card-title").textContent
-                && object[i].size === bool_size1
-                && object[i].doughType === bool_dough1)
-            {
-                flag = true;
-                ind = i;
-            }
+        if (document.querySelector(".session").value === '') {
+            alert("Для начала войдите в аккаунт");
         }
-        if (flag) object[ind].counter = object[ind].counter + 1;
         else {
-            let pizza_index = pizza_card.querySelector(".busket-button-text").textContent.search(" ");
-            let pizza_price = parseFloat(pizza_card.querySelector(".busket-button-text").textContent.substring(0, pizza_index));
-            let addition = 0;
-            let w = 0;
-            let bool_size = true, bool_dough = true;
-            if (pizza_card.querySelector(".size-select").options[0].textContent === "Большой") bool_size = false;
-            if (pizza_card.querySelector(".dough-select").options[0].textContent === "Толстое") bool_size = false;
-            if (!bool_dough && bool_size) {
-                addition = 1;
-                w = 50;
+            const element_id = element.id.charAt(7);
+            const price_name = "price-" + element_id;
+            let pizza_card = document.getElementById("pizza-" + element_id);
+            let object = JSON.parse(localStorage["pizza"]);
+            let flag = false;
+            let ind = 0;
+            let bool_size1 = true, bool_dough1 = true;
+            if (pizza_card.querySelector(".size-select").options[0].textContent === "Большой") bool_size1 = false;
+            if (pizza_card.querySelector(".dough-select").options[0].textContent === "Толстое") bool_size1 = false;
+            for (let i = 0; i < object.length; i++) {
+                if (object[i].name === pizza_card.querySelector(".pizza-card-title").textContent
+                    && object[i].size === bool_size1
+                    && object[i].doughType === bool_dough1)
+                {
+                    flag = true;
+                    ind = i;
+                }
             }
-            if (bool_dough && !bool_size) {
-                addition = 2;
-                w = 150;
+            if (flag) object[ind].counter = object[ind].counter + 1;
+            else {
+                let pizza_index = pizza_card.querySelector(".busket-button-text").textContent.search(" ");
+                let pizza_price = parseFloat(pizza_card.querySelector(".busket-button-text").textContent.substring(0, pizza_index));
+                let addition = 0;
+                let w = 0;
+                let bool_size = true, bool_dough = true;
+                if (pizza_card.querySelector(".size-select").options[0].textContent === "Большой") bool_size = false;
+                if (pizza_card.querySelector(".dough-select").options[0].textContent === "Толстое") bool_size = false;
+                if (!bool_dough && bool_size) {
+                    addition = 1;
+                    w = 50;
+                }
+                if (bool_dough && !bool_size) {
+                    addition = 2;
+                    w = 150;
+                }
+                if (!bool_dough && !bool_size) {
+                    addition = 3;
+                    w = 200;
+                }
+                let pizza_item = {
+                    id: ids[element_id - 1] + addition,
+                    name: pizza_card.querySelector(".pizza-card-title").textContent,
+                    size: bool_size,
+                    doughType: bool_dough,
+                    counter: 1,
+                    weight: 500 + w,
+                    ingredients: pizza_card.querySelector(".ingredients-text").textContent,
+                    price: pizza_price.toFixed(2)
+                }
+                object.push(pizza_item);
             }
-            if (!bool_dough && !bool_size) {
-                addition = 3;
-                w = 200;
-            }
-            let pizza_item = {
-                id: ids[element_id - 1] + addition,
-                name: pizza_card.querySelector(".pizza-card-title").textContent,
-                size: bool_size,
-                doughType: bool_dough,
-                counter: 1,
-                weight: 500 + w,
-                ingredients: pizza_card.querySelector(".ingredients-text").textContent,
-                price: pizza_price.toFixed(2)
-            }
-            object.push(pizza_item);
+            localStorage["pizza"] = JSON.stringify(object);
+            console.log(object);
+
+            var text = document.getElementById(price_name).textContent;
+            var index = text.search(" ");
+            var busket_index = busket.textContent.search(" ");
+            const prev_price = parseFloat(busket.textContent.substring(0, busket_index));
+            var new_price = prev_price + parseFloat(document.getElementById(price_name).textContent.substring(0, index));
+            localStorage["busket-price"] = new_price.toFixed(2);
+            busket.textContent = new_price.toFixed(2) + " BYN";
+            const new_quantity = parseInt(busket_quantity.textContent) + 1;
+            localStorage['busket-quantity'] = new_quantity;
+
+            busket_quantity.textContent = new_quantity;
         }
-        localStorage["pizza"] = JSON.stringify(object);
-        console.log(object);
-
-        var text = document.getElementById(price_name).textContent;
-        var index = text.search(" ");
-        var busket_index = busket.textContent.search(" ");
-        const prev_price = parseFloat(busket.textContent.substring(0, busket_index));
-        var new_price = prev_price + parseFloat(document.getElementById(price_name).textContent.substring(0, index));
-        localStorage["busket-price"] = new_price.toFixed(2);
-        busket.textContent = new_price.toFixed(2) + " BYN";
-        const new_quantity = parseInt(busket_quantity.textContent) + 1;
-        localStorage['busket-quantity'] = new_quantity;
-
-        busket_quantity.textContent = new_quantity;
     });
 });
 
 drink_buskets.forEach(element => {
     element.addEventListener("click", function() {
-        var button_index = element.id.search("-");
-        const element_id = element.id.substring(button_index + 1);
-        const price_name = "dprice-" + element_id;
-
-        let drink_card = document.getElementById("drink-" + element_id);
-        let object = JSON.parse(localStorage["drinks"]);
-        let flag = false;
-        let ind = 0;
-        for (let i = 0; i < object.length; i++) {
-            if ((object[i].name + ", " + object[i].capacity) === drink_card.querySelector(".drink-card-title").textContent)
-            {
-                flag = true;
-                ind = i;
-            }
+        if (document.querySelector(".session").value === '') {
+            alert("Для начала войдите в аккаунт");
         }
-        if (flag) object[ind].counter = object[ind].counter + 1;
         else {
-            let name_index = drink_card.querySelector(".drink-card-title").textContent.search(",");
-            let drink_index = drink_card.querySelector(".drink-price-button-text").textContent.search(" ");
-            let drink_price = parseFloat(drink_card.querySelector(".drink-price-button-text").textContent.substring(0, drink_index));
-            let drink_item = {
-                id: element_id,
-                name: drink_card.querySelector(".drink-card-title").textContent.substring(0, name_index),
-                counter: 1,
-                capacity: capacities[element_id - 1],
-                price: drink_price.toFixed(2)
+            var button_index = element.id.search("-");
+            const element_id = element.id.substring(button_index + 1);
+            const price_name = "dprice-" + element_id;
+
+            let drink_card = document.getElementById("drink-" + element_id);
+            let object = JSON.parse(localStorage["drinks"]);
+            let flag = false;
+            let ind = 0;
+            for (let i = 0; i < object.length; i++) {
+                if ((object[i].name + ", " + object[i].capacity) === drink_card.querySelector(".drink-card-title").textContent)
+                {
+                    flag = true;
+                    ind = i;
+                }
             }
-            object.push(drink_item);
+            if (flag) object[ind].counter = object[ind].counter + 1;
+            else {
+                let name_index = drink_card.querySelector(".drink-card-title").textContent.search(",");
+                let drink_index = drink_card.querySelector(".drink-price-button-text").textContent.search(" ");
+                let drink_price = parseFloat(drink_card.querySelector(".drink-price-button-text").textContent.substring(0, drink_index));
+                let drink_item = {
+                    id: element_id,
+                    name: drink_card.querySelector(".drink-card-title").textContent.substring(0, name_index),
+                    counter: 1,
+                    capacity: capacities[element_id - 1],
+                    price: drink_price.toFixed(2)
+                }
+                object.push(drink_item);
+            }
+            localStorage["drinks"] = JSON.stringify(object);
+
+            console.log(object);
+
+            var text = document.getElementById(price_name).textContent;
+            var index = text.search(" ");
+            var busket_index = busket.textContent.search(" ");
+            const prev_price = parseFloat(busket.textContent.substring(0, busket_index));
+            var new_price = prev_price + parseFloat(document.getElementById(price_name).textContent.substring(0, index));
+            localStorage["busket-price"] = new_price.toFixed(2);
+            busket.textContent = new_price.toFixed(2) + " BYN";
+            const new_quantity = parseInt(busket_quantity.textContent) + 1;
+            localStorage['busket-quantity'] = new_quantity;
+
+            busket_quantity.textContent = new_quantity;
         }
-        localStorage["drinks"] = JSON.stringify(object);
-
-        console.log(object);
-
-        var text = document.getElementById(price_name).textContent;
-        var index = text.search(" ");
-        var busket_index = busket.textContent.search(" ");
-        const prev_price = parseFloat(busket.textContent.substring(0, busket_index));
-        var new_price = prev_price + parseFloat(document.getElementById(price_name).textContent.substring(0, index));
-        localStorage["busket-price"] = new_price.toFixed(2);
-        busket.textContent = new_price.toFixed(2) + " BYN";
-        const new_quantity = parseInt(busket_quantity.textContent) + 1;
-        localStorage['busket-quantity'] = new_quantity;
-
-        busket_quantity.textContent = new_quantity;
     });
 });
